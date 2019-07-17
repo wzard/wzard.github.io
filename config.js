6679f2530e59743e81f9a500a5c2ef9e81fda2a7
Siddharth Kanungo <sid.dharth.soka@gmail.com>
2019-07-17 21:23:13 +0530

    Setup Completed

---
+++ config.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+	url: 'https://lumen.netlify.com',
+	pathPrefix: '/',
+	title: 'The Housebuilder',
+	subtitle: 'Mostly about sounding smart to random internet strangers !',
+	copyright: '© All rights reserved for The Housebuilder',
+	disqusShortname: '',
+	postsPerPage: 5,
+	googleAnalyticsId: 'UA-131802752-1',
+	menu: [
+		{
+			label: 'Articles',
+			path: '/'
+		},
+		{
+			label: 'About me',
+			path: '/pages/about'
+		},
+		{
+			label: 'Contact me',
+			path: '/pages/contacts'
+		}
+	],
+	author: {
+		name: 'Siddharth Kanungo',
+		photo: '/profile.jpg',
+		bio: 'Creating Primer. A Dialog-based intelligent tutoring system.',
+		contacts: {
+			email: 'siddharth@primerlabs.io',
+			telegram: '#',
+			twitter: '_Wzard',
+			github: 'wzard',
+			rss: '#',
+			vkontakte: '#'
+		}
+	}
+};
---
+++ content/pages/contacts.md
@@ -0,0 +1,8 @@
+---
+title: "Contact me"
+template: "page"
+---
+
+If you want to get in touch with me, the best way is to find me at Twitter or send me an Email.
+
+I am not looking for work right now. 
